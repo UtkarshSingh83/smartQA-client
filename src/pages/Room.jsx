a1337619d8da5c6d7c2579bbb317396673511e22
UtkarshSingh83 <121507514+UtkarshSingh83@users.noreply.github.com>
2025-07-16 16:52:16 +0530

    Room

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import Question from "./Question";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { serverEndpoint } from "../config/appConfig";
+import socket from "../config/socket";
+// import { error } from "console";
 
 function Room() {
+  const { code } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState({});
+  const [room, setRoom] = useState(null);
+  const [questions, setQuestions] = useState([]);
+
+  const fetchRoom = async () => {
+    try {
+      const response = await axios.get(`${serverEndpoint}/room/${code}`, {
+        withCredentials: true
+      });
+      setRoom(response.data);
+    } catch (error) {
+      console.log(error);
+      setErrors({ message: 'Unable to fetch room details, please try again' });
+    }
+  };
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await axios.get(`${ serverEndpoint }/room/${code}/question`, {
+        withCredentials: true
+      });
+      setQuestions(response.data);
+    } catch (error) {
+      console.log(error);
+      setErrors({ message: 'Unable to fetch questions, please try again' });
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await fetchRoom();
+      await fetchQuestions();
+      setLoading(false);
+    };
+
+
+    fetchData();
+
+    socket.emit("join-room", code);
+
+    socket.on("new-question", (question) => {
+      setQuestions((prev) => [question, ...prev]);
+    });
+
+    return () => {
+      socket.off("new-question");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="conatiner text-center py-5">
+        <h3>Loading</h3>
+        <p>Fetching room details...</p>
+      </div>
+    );
+  }
+
+
+  if (errors.message) {
+    return (
+      <div className="conatiner text-center py-5">
+        <h3>Error</h3>
+        <p>Fetching room details...</p>
+      </div>
+    );
+  }
+
   return (
-     <div className="container py-5">
-        <h2>Room </h2>
+    <div className="container py-5">
+      <h2 className="mb-2">Room {code} </h2>
+      <div className="row">
+        <div className="col-auto">
+          <ul className="list-group">
+            {questions.map((ques) => (
+              <li key={ques._id} className="list-group-item">
+                {ques.content}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <Question roomCode={code} />
     </div>
-  )
+  );
 }
 
-export default Room
+export default Room;
